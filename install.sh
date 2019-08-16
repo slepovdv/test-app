26f99cfb04ce87f02e031c3e2eddc948c76a0731
Denis Slepov <tmnkun@gmail.com>
2019-08-16 22:31:26 +0300

    Add nginx exporter and dashboard

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 docker volume create pgdata
 docker volume create pmdata
 docker volume create grdata
+docker volume create mtaildata
+#mkdir -p /var/lib/docker/volumes/nginxdata/_data/mtail
+#touch /var/lib/docker/volumes/nginxdata/_data/mtail/access.log
 docker-compose up -d
 
-ansible-galaxy install nginxinc.nginx
-ansible-playbook nginx_deploy.yml -K
+#ansible-galaxy install nginxinc.nginx
+#ansible-playbook nginx_deploy.yml -K
