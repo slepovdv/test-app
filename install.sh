18f2968f9dff4e95a2f262470c481957a2c64b03
Denis Slepov <tmnkun@gmail.com>
2019-08-18 23:02:54 +0300

    Update Readme.md

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ docker volume create pgdata
 docker volume create pmdata
 docker volume create grdata
 docker volume create mtaildata
-#mkdir -p /var/lib/docker/volumes/nginxdata/_data/mtail
-#touch /var/lib/docker/volumes/nginxdata/_data/mtail/access.log
 docker-compose up -d
 
 #ansible-galaxy install nginxinc.nginx
